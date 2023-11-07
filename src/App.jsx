921a931caa1a53a8f55be5642a9aebabb95d1b1e
Luka Talakvadze <lukatalakvadze@gmail.com>
2023-11-07 23:44:37 +0400

    created dark/light mode function

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,74 @@ import React, { useState } from "react";
 import "./style.css";
 
 function App() {
+  const [iconSrc, setIconSrc] = useState("./src/assets/icon-moon.svg"); // Starting with the moon icon
+  const [darkMode, setDarkMode] = useState(false);
+
+  //Creating a function which toggles dark and light mode styles on elements
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+
+    if (darkMode) {
+      setIconSrc("./src/assets/icon-moon.svg");
+      document
+        .querySelector(".heading-div")
+        .classList.remove("dark-mode-backgroundImage");
+      document.querySelector("body").classList.remove("dark-background-color");
+      document
+        .getElementById("create-new")
+        .classList.remove("dark-background-color");
+      document
+        .querySelector(".create-todo")
+        .classList.remove("dark-background-color");
+      document
+        .querySelector(".last-window")
+        .classList.remove("dark-background-color", "boxShadow-dark");
+      document
+        .querySelector(".last-window-desktop")
+        .classList.remove("dark-background-color", "boxShadow-dark");
+      document
+        .querySelector(".footer-div")
+        .classList.remove("dark-background-color", "boxShadow-dark");
+      const elements = document.querySelectorAll(".page-word");
+      elements.forEach((element) => {
+        element.classList.remove("dark-hover");
+        element.classList.add("light-hover");
+      });
+    } else {
+      setIconSrc("./src/assets/icon-sun.svg");
+      document
+        .querySelector(".heading-div")
+        .classList.add("dark-mode-backgroundImage");
+      document.querySelector("body").classList.add("dark-background-color");
+      document
+        .getElementById("create-new")
+        .classList.add("dark-background-color");
+      document
+        .querySelector(".create-todo")
+        .classList.add("dark-background-color");
+      document
+        .querySelector(".last-window")
+        .classList.add("dark-background-color", "boxShadow-dark");
+      document
+        .querySelector(".last-window-desktop")
+        .classList.add("dark-background-color", "boxShadow-dark");
+      document
+        .querySelector(".footer-div")
+        .classList.add("dark-background-color", "boxShadow-dark");
+      const elements = document.querySelectorAll(".page-word");
+      elements.forEach((element) => {
+        element.classList.add("dark-hover");
+        element.classList.remove("light-hover");
+      });
+    }
+  };
+
   return (
     <div className="main">
       <div className="heading-div">
         <div className="logo-and-moon">
           <img className="logo" src="./src/assets/logo.svg" alt="Logo" />
-          <img className="moon" src="./src/assets/icon-moon.svg" alt="Moon" />
+          <img className="moon" src={iconSrc} onClick={toggleDarkMode} />
         </div>
         <form id="create-new">
           <div className="circle-and-input">
@@ -51,13 +113,13 @@ function App() {
           <p className="clear-completed">Clear Completed</p>
         </div>
         <div className="last-window-desktop">
-          <p className="left-items">items left</p>
+          <p className="left-items page-word">items left</p>
           <div className="footer-div-desktop">
-            <p className="all">All</p>
-            <p className="active">Active</p>
-            <p className="completed">Completed</p>
+            <p className="all page-word">All</p>
+            <p className="active page-word">Active</p>
+            <p className="completed page-word">Completed</p>
           </div>
-          <p className="clear-completed">Clear Completed</p>
+          <p className="clear-completed page-word">Clear Completed</p>
         </div>
       </div>
 
