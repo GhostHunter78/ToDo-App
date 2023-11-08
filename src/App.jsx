05411f6df718b53ba7cdef5d281da183eafa3177
Luka Talakvadze <lukatalakvadze@gmail.com>
2023-11-08 13:39:15 +0400

    Created function that adds  new tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./style.css";
 
 function App() {
   const [iconSrc, setIconSrc] = useState("./src/assets/icon-moon.svg"); // Starting with the moon icon
   const [darkMode, setDarkMode] = useState(false);
+  const [allTasksArray, setAllTasksArray] = useState([]);
+  const [taskText, setTaskText] = useState("");
+  const [tasks, setTasks] = useState([]);
 
   //Creating a function which toggles dark and light mode styles on elements
   const toggleDarkMode = () => {
@@ -31,9 +34,17 @@ function App() {
         .querySelector(".footer-div")
         .classList.remove("dark-background-color", "boxShadow-dark");
       const elements = document.querySelectorAll(".page-word");
-      elements.forEach((element) => {
-        element.classList.remove("dark-hover");
-        element.classList.add("light-hover");
+      const pageWords = document.querySelectorAll(".page-word");
+      pageWords.forEach((pageWord) => {
+        pageWord.classList.remove("dark-hover");
+        pageWord.classList.add("light-hover");
+        const createdTaskDivs = document.querySelectorAll(".created-task-div");
+        createdTaskDivs.forEach((createdTaskDiv) => {
+          createdTaskDiv.classList.remove(
+            "dark-background-color",
+            "boxShadow-dark"
+          );
+        });
       });
     } else {
       setIconSrc("./src/assets/icon-sun.svg");
@@ -56,12 +67,42 @@ function App() {
       document
         .querySelector(".footer-div")
         .classList.add("dark-background-color", "boxShadow-dark");
-      const elements = document.querySelectorAll(".page-word");
-      elements.forEach((element) => {
-        element.classList.add("dark-hover");
-        element.classList.remove("light-hover");
+      const pageWords = document.querySelectorAll(".page-word");
+      pageWords.forEach((pageWord) => {
+        pageWord.classList.add("dark-hover");
+        pageWord.classList.remove("light-hover");
+
+        const createdTaskDivs = document.querySelectorAll(".created-task-div");
+        createdTaskDivs.forEach((createdTaskDiv) => {
+          createdTaskDiv.classList.add(
+            "dark-background-color",
+            "boxShadow-dark"
+          );
+        });
       });
     }
+  };
+
+  //Creating a function which creates new tasks on clicking create-todo-circle and adds it in allTasksArray
+  const handleCreateCircleClick = () => {
+    if (taskText.trim() !== "") {
+      const newTask = {
+        text: taskText,
+      };
+      setTasks([...tasks, newTask]);
+      setAllTasksArray([...allTasksArray, newTask]);
+      setTaskText(""); // Clear the input field after adding the task
+    }
+  };
+
+  // useEffect(() => {
+  //   console.log("allTasksArray:", allTasksArray);
+  // }, [allTasksArray]);
+
+  const handleCrossIconClick = (index) => {
+    const updatedTasks = [...tasks];
+    updatedTasks.splice(index, 1);
+    setTasks(updatedTasks);
   };
 
   return (
@@ -73,7 +114,10 @@ function App() {
         </div>
         <form id="create-new">
           <div className="circle-and-input">
-            <div className="circle create-todo-circle">
+            <div
+              className="circle create-todo-circle "
+              onClick={handleCreateCircleClick}
+            >
               <img
                 className="check-icon"
                 src="./src/assets/icon-check.svg"
@@ -84,30 +128,48 @@ function App() {
               type="text"
               className="create-todo"
               placeholder="Create a new todo…"
+              value={taskText}
+              onChange={(e) => setTaskText(e.target.value)}
             />
           </div>
         </form>
       </div>
       <div className="content">
-        {/* <div className="created-task-div">
-          <div className="circle-text-cross">
-            <div className="circle-and-text">
-              <div className="circle">
-                <img
-                  className="check-icon"
-                  src="./src/assets/icon-check.svg"
-                  alt="Check Icon"
-                />
+        {tasks.map((task, index) => (
+          <div
+            key={index}
+            className="created-task-div"
+            style={{
+              backgroundColor: darkMode ? "#25273d" : "#fff",
+            }}
+          >
+            <div className="circle-text-cross">
+              <div className="circle-and-text">
+                <div className="circle">
+                  <img
+                    className="check-icon"
+                    src="./src/assets/icon-check.svg"
+                    alt="Check Icon"
+                  />
+                </div>
+                <p
+                  className="created-task"
+                  style={{
+                    color: darkMode ? "#fff" : "#494c6b",
+                  }}
+                >
+                  {task.text}
+                </p>
               </div>
-              <p className="created-task"></p>
+              <img
+                className="cross"
+                src="./src/assets/icon-cross.svg"
+                alt="Cross Icon"
+                onClick={handleCrossIconClick}
+              />
             </div>
-            <img
-              className="cross"
-              src="./src/assets/icon-cross.svg"
-              alt="Cross Icon"
-            />
           </div>
-        </div> */}
+        ))}
         <div className="last-window">
           <p className="left-items">items left</p>
           <p className="clear-completed">Clear Completed</p>
